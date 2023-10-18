5e6d3f5ededb27b2020be13036edafe3fbc07ddf
ACTwoSeven <andrescrn27@hotmail.com>
2023-10-18 16:27:23 -0500

    Commit backend 18/10 auth casi completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pathfinder/Pages/auth_page.dart';
+import 'Pages/RegisterPage.dart';
+import 'Pages/LoginPage.dart';
+import 'firebase/firebase_options.dart';
 
-import 'RegisterPage.dart';
-import 'firebase_options.dart';
 
-void main() async{
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -15,43 +18,9 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Página principal',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Página principal'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  // Acción para iniciar sesión
-                },
-                child: Text('Iniciar sesión'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('Entrando a registro');
-                  Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-              );
-              },
-                child: Text('Registrarse'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Acción para ingresar como invitado
-                },
-                child: Text('Ingresar como invitado'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home : AuthPage());
   }
 }
